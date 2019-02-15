d048918b1a7044f6c0fc30e793ed406fb87b0fa7
Zaharia <0274237@students.dadeschools.net>
2019-02-15 11:51:19 -0500

    worked on keys and on css

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// Variables
+// VARIABLES
 let car = {
     make: 'McLaren',
     model: 'Formula 1',
@@ -8,24 +8,64 @@ let car = {
 
     driveToWork(){
         alert(` Old Mileage: ${this.mileage}`);
-        this.mileage = this.mileage + 10;
+        this.mileage += 10;
         alert(` Old Mileage: ${this.mileage}`);
+    },
+
+    driveAroundUSA(){
+        alert(` Old Mileage: ${this.mileage}`);
+        this.mileage += 7000;
+        alert(` Old Mileage: ${this.mileage}`);
+        alert('Car needs a tuneup!');
+        this.isWorking = false;
+    },
+
+    getTuneUP(){
+        alert('Car is fixed and ready to go!');
+        this.isWorking = true;
+    },
+
+    honk(){
+        alert('beep! honk!');
     },
 }
 
-// Functions
-// Logs all our current stats to the console
-const rewriteStats() => {
+    
 
+
+// FUNCTIONS
+// Logs all our current stats to the console
+const rewriteStats=()=>{
+    console.log(`Make: ${car.make}`);
+    console.log(`Model: ${car.model}`);
+    console.log(`Color: ${car.color}`);
+    console.log(`Mileage: ${car.mileage}`);
+    console.log(`IsWorking: ${car.isWorking}`);
+    console.log(`------------------------------------`);
 }
 
-//Main process
+//MAIN PROCESS
 //Captures keyboard input. Depeding on the letter pressed it will "call" different functions.
-const document.onkeyup(event)=>{
-    let letter = String.fromCharCode(event.keyCode).toLocaleLowerCase();
+ document.onkeyup = (event) => {
+    let letter = String.fromCharCode(event.keyCode).toLowerCase();
 
     if (letter === 'd'){
         car.driveToWork();
+        rewriteStats();
+    }
+
+    if (letter === 'r'){
+        car.driveAroundUSA();
+        rewriteStats();
+    }
+
+    if (letter === 't'){
+        car.getTuneUP();
+        rewriteStats();
+    }
+
+    if (letter === 'h'){
+        car.honk();
         rewriteStats();
     }
 }
